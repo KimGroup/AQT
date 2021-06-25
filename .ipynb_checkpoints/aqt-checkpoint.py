0ff8bbb4a63543b2eaac2fb8951172d909ef35ac
pjc277 <pjc277@cornell.edu>
2021-06-25 10:50:14 -0400

    new file:   .ipynb_checkpoints/ann-checkpoint.py 	modified:   .ipynb_checkpoints/aqt-checkpoint.ipynb 	modified:   .ipynb_checkpoints/aqt-checkpoint.py 	new file:   .ipynb_checkpoints/dm_3d_plot-checkpoint.ipynb 	modified:   .ipynb_checkpoints/noise_1-checkpoint.ipynb 	new file:   .ipynb_checkpoints/noise_1-checkpoint.py 	modified:   .ipynb_checkpoints/noise_2-checkpoint.ipynb 	new file:   .ipynb_checkpoints/povm-checkpoint.py 	new file:   .ipynb_checkpoints/sample_pure_state-checkpoint.py 	modified:   aqt.ipynb 	modified:   aqt.py 	modified:   dm_3d_plot.ipynb 	new file:   figures/dm_aqt.svg 	new file:   figures/dm_aqt_6.svg 	new file:   figures/dm_dicke_3_1.svg 	new file:   figures/dm_dicke_6_1.svg 	new file:   figures/dm_dicke_6_2.svg 	new file:   figures/dm_ibmq.svg 	new file:   figures/noise_2.pdf 	new file:   ghz_3_ibmq/3_2700_10-2-64-4.mod 	new file:   ghz_3_ibmq/3_2700_10-2-64-4_dm.npy 	new file:   ghz_3_ibmq/3_2700_10-2-64-4_loss.npy 	new file:   ghz_3_ibmq/3_2700_10-2-64-4_pt.npy 	new file:   ghz_6/6_72900.npy 	new file:   ghz_6/6_72900_pt.npy 	modified:   ghz_6/6_dm.npy 	new file:   ghz_6/6_st.npy 	modified:   noise_1.ipynb 	modified:   noise_2.ipynb 	modified:   sample_pure_state.py

--- .ipynb_checkpoints/aqt-checkpoint.py
+++ .ipynb_checkpoints/aqt-checkpoint.py
@@ -1,71 +1,78 @@
 import time
-from fidelity import *
-from povm import *
-from ann import *
+import numpy as np
+import torch
 
+import fidelity as F
+import povm as P
+import ann as A
+import sys
 
 
 # Basic parameters
 
-path = 'noise_2/0.3'
+def AQT(datapath, Nq, Nep, Nl=2, dmodel=64, Nh=4, save_model=True, save_loss=True, save_pt=True, save_dm=True):
 
-Nq = 3
+    povm = P.POVM('pauli6')
+    Na = povm.Na
 
-Ns = 3000
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
 
-povm = POVM('pauli6')
-Na = povm.Na
+    # Load data
 
-# Hyperparameters
+    data = np.load('{}.npy'.format(datapath))
+    np.random.shuffle(data)
 
-Nl, dmodel, Nh = 2, 64, 4
-Nep = 100
-
-print(torch.cuda.is_available())
-device = torch.device("cuda:0")
-
-
-
-# Load data
-
-data = np.load('{}/{}_{}.npy'.format(path, Nq, Ns))
-np.random.shuffle(data)
-
-split = 0.8
-traindata = data[:int(len(data)*split)]
-testdata = data[int(len(data)*split):]
+    split = 0.8
+    traindata = data[:int(len(data)*split)]
+    testdata = data[int(len(data)*split):]
 
 
-# Train model
+    # Train model
 
-model_filetag = '{}/{}_{}_{}-{}-{}-{}'.format(path, Nq, Ns, Nep, Nl, dmodel, Nh)
-
-
-model = InitializeModel(Nq, Nlayer=Nl, dmodel=dmodel, Nh=Nh, Na=Na).to(device)
-
-t = time.time()
-model, loss = TrainModel(model, traindata, testdata, device, batch_size=50,lr=1e-4,Nep=Nep)
-print('Took %f minutes'%((time.time()-t)/60))
-
-model.to('cpu')
-torch.save(model, '{}.mod'.format(model_filetag))
-np.save('{}_loss.npy'.format(model_filetag), loss)
-
-# Build POVM probability table
-
-pt_model = POVMProbTable(model)
-
-np.save('{}_pt.npy'.format(model_filetag), pt_model)
-
-# Reconstruct density matrix
-
-dm8 = np.zeros((8, 2**Nq, 2**Nq), dtype=complex)
-
-for xyz in range(8):
-    dm8[xyz] = GetDMFull(pt_model, Nq, POVM('pauli6', xyz))
+    model_filetag = '{}_{}-{}-{}-{}'.format(datapath, Nep, Nl, dmodel, Nh)
 
 
-_, negeig, dm_model = GetBestDM(dm8)
+    model = A.InitializeModel(Nq, Nlayer=Nl, dmodel=dmodel, Nh=Nh, Na=Na).to(device)
 
-np.save('{}_dm.npy'.format(model_filetag), dm_model)
+    t = time.time()
+    model, loss = A.TrainModel(model, traindata, testdata, device, batch_size=50,lr=1e-4,Nep=Nep)
+    print('Took %f minutes'%((time.time()-t)/60))
 
+    model.to('cpu')
+    if save_model:
+        torch.save(model, '{}.mod'.format(model_filetag))
+    if save_loss:
+        np.save('{}_loss.npy'.format(model_filetag), loss)
+
+    # Build POVM probability table
+
+    pt_model = F.POVMProbTable(model)
+
+    if save_pt:
+        np.save('{}_pt.npy'.format(model_filetag), pt_model)
+
+    # Reconstruct density matrix
+
+    dm8 = np.zeros((8, 2**Nq, 2**Nq), dtype=complex)
+
+    for xyz in range(8):
+        dm8[xyz] = F.GetDMFull(pt_model, Nq, P.POVM('pauli6', xyz))
+
+
+    _, negeig, dm_model = F.GetBestDM(dm8)
+
+    if save_dm:
+        np.save('{}_dm.npy'.format(model_filetag), dm_model)
+
+    return dm_model
+
+if __name__ == '__main__':
+
+    datapath = 'dicke_6_2/6_72900'
+    Nq = 6
+    Nep = 100
+    
+    AQT(datapath, Nq, Nep)
--- aqt.py
+++ aqt.py
@@ -1,7 +1,10 @@
 import time
-from fidelity import *
-from povm import *
-from ann import *
+import numpy as np
+import torch
+
+import fidelity as F
+import povm as P
+import ann as A
 import sys
 
 
@@ -9,7 +12,7 @@ import sys
 
 def AQT(datapath, Nq, Nep, Nl=2, dmodel=64, Nh=4, save_model=True, save_loss=True, save_pt=True, save_dm=True):
 
-    povm = POVM('pauli6')
+    povm = P.POVM('pauli6')
     Na = povm.Na
 
     if torch.cuda.is_available():
@@ -32,10 +35,10 @@ def AQT(datapath, Nq, Nep, Nl=2, dmodel=64, Nh=4, save_model=True, save_loss=Tru
     model_filetag = '{}_{}-{}-{}-{}'.format(datapath, Nep, Nl, dmodel, Nh)
 
 
-    model = InitializeModel(Nq, Nlayer=Nl, dmodel=dmodel, Nh=Nh, Na=Na).to(device)
+    model = A.InitializeModel(Nq, Nlayer=Nl, dmodel=dmodel, Nh=Nh, Na=Na).to(device)
 
     t = time.time()
-    model, loss = TrainModel(model, traindata, testdata, device, batch_size=50,lr=1e-4,Nep=Nep)
+    model, loss = A.TrainModel(model, traindata, testdata, device, batch_size=50,lr=1e-4,Nep=Nep)
     print('Took %f minutes'%((time.time()-t)/60))
 
     model.to('cpu')
@@ -46,7 +49,7 @@ def AQT(datapath, Nq, Nep, Nl=2, dmodel=64, Nh=4, save_model=True, save_loss=Tru
 
     # Build POVM probability table
 
-    pt_model = POVMProbTable(model)
+    pt_model = F.POVMProbTable(model)
 
     if save_pt:
         np.save('{}_pt.npy'.format(model_filetag), pt_model)
@@ -56,10 +59,10 @@ def AQT(datapath, Nq, Nep, Nl=2, dmodel=64, Nh=4, save_model=True, save_loss=Tru
     dm8 = np.zeros((8, 2**Nq, 2**Nq), dtype=complex)
 
     for xyz in range(8):
-        dm8[xyz] = GetDMFull(pt_model, Nq, POVM('pauli6', xyz))
+        dm8[xyz] = F.GetDMFull(pt_model, Nq, P.POVM('pauli6', xyz))
 
 
-    _, negeig, dm_model = GetBestDM(dm8)
+    _, negeig, dm_model = F.GetBestDM(dm8)
 
     if save_dm:
         np.save('{}_dm.npy'.format(model_filetag), dm_model)
@@ -68,8 +71,8 @@ def AQT(datapath, Nq, Nep, Nl=2, dmodel=64, Nh=4, save_model=True, save_loss=Tru
 
 if __name__ == '__main__':
 
-    datapath = 'dicke_6_2/6_72900'
-    Nq = 6
-    Nep = 100
+    datapath = 'ghz_3_ibmq/3_2700'
+    Nq = 3
+    Nep = 10
     
     AQT(datapath, Nq, Nep)
